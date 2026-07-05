7b8373372749fd82d72aa937e6ba5e38324ec17f
Martim Gil <a.martimgil@gmail.com>
2026-07-05 22:52:00 +0100

    feat: enhance type annotations for __table_args__ in Base model

--- api-rally/app/models/base.py
+++ api-rally/app/models/base.py
@@ -1,5 +1,5 @@
 import re
-from typing import Any
+from typing import Any, ClassVar
 
 from sqlalchemy.orm import DeclarativeBase, declared_attr
 from app.core.config import settings
@@ -12,4 +12,4 @@ class Base(DeclarativeBase):
         names = re.findall("[A-Z][^A-Z]*", cls.__name__)
         return "_".join(names).lower()
 
-    __table_args__: tuple[Any, ...] = ({"schema": settings.SCHEMA_NAME},)
+    __table_args__: ClassVar[Any] = ({"schema": settings.SCHEMA_NAME},)
