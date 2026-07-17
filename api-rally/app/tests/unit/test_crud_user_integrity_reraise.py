115d958c2637745a8e2e20ce9f64b9dd03a47031
Martim Gil <a.martimgil@gmail.com>
2026-07-18 00:53:28 +0100

    refactor: simplify user update tests by reusing UserUpdate instance

--- api-rally/app/tests/unit/test_crud_user_integrity_reraise.py
+++ api-rally/app/tests/unit/test_crud_user_integrity_reraise.py
@@ -33,13 +33,15 @@ async def test_update_reraises_when_orig_is_none(pg_session):
         "app.crud.base.CRUDBase.update",
         new=AsyncMock(side_effect=IntegrityError("x", None, None)),
     ):
+        obj_in = UserUpdate(name="New")
         with pytest.raises(IntegrityError):
-            await crud_user.update(pg_session, id=created.id, obj_in=UserUpdate(name="New"))
+            await crud_user.update(pg_session, id=created.id, obj_in=obj_in)
 
 
 async def test_update_reraises_when_error_does_not_match_team_fk(pg_session):
     created = await crud_user.create(pg_session, obj_in=UserCreate(name="Update Unrelated"))
     unrelated = IntegrityError("x", None, RuntimeError("some other constraint violation"))
     with patch("app.crud.base.CRUDBase.update", new=AsyncMock(side_effect=unrelated)):
+        obj_in = UserUpdate(name="New")
         with pytest.raises(IntegrityError):
-            await crud_user.update(pg_session, id=created.id, obj_in=UserUpdate(name="New"))
+            await crud_user.update(pg_session, id=created.id, obj_in=obj_in)
