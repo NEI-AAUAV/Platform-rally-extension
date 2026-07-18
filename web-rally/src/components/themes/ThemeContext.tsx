928ec9c1b4839229ae058e7bdd0877e9c8814e78
Martim Gil <a.martimgil@gmail.com>
2026-07-18 01:24:18 +0100

    refactor: update import path for theme components in ThemeContext

--- web-rally/src/components/themes/ThemeContext.tsx
+++ web-rally/src/components/themes/ThemeContext.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react-refresh/only-export-components -- file exports hooks alongside provider intentionally */
 // Theme context and hook for Rally components
 import { createContext, useContext, useState, useEffect, useMemo, type ReactNode } from "react";
-import { getThemeComponents, type ThemeName, type ThemeComponents } from "./themeRegistry";
+import { getThemeComponents, type ThemeName, type ThemeComponents } from "@/components";
 import useRallySettings from "@/hooks/useRallySettings";
 
 interface ThemeContextType {
